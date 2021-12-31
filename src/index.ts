007d8d4a153e3720e0194edebb9a500d609f8c7e
A1yangshuang <593277314@qq.com>
2021-12-31 15:47:05 +0800

    Updates

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Pagination } from './pagination/index';
-export { default as Button } from './button/index';
-export { default as Popover } from './popover/index';
-// export { default as Select } from './select/index';
-export { default as Spin } from './spin/index';
-export { default as Tooltip } from './tooltip/index';
